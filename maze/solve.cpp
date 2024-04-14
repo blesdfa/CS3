88995b0caa5c25014c01a7bebf3009ba9fa18a68
David Torres <david.torres10@utrgv.edu>
2024-04-14 15:03:13 -0500

    finished maze with portals

--- maze/solve.cpp
+++ maze/solve.cpp
@@ -30,38 +30,16 @@ void addBasicEdge(string a, string b, unordered_map<string, Vertex*> &m, int w) 
     addDirectedEdge(b, a, m, w);
 }
 
-void bfs(string s, unordered_map<string, Vertex*> &m) {
-    clearData(m);
-    Vertex* start = m[s];
-    queue<Vertex*> q;
-    q.push(start);
-    start->marked = true;
-    
-    while(!q.empty()) {
-        Vertex* temp = q.front();
-        q.pop();
-
-        for (auto x : temp->neighs) {
-            if (!x.first->marked) {
-                x.first->marked = true;
-                q.push(x.first);
-                x.first->bc = temp;
-            }
-        }
-    }
-}
 
 void dijsktras(unordered_map<string, Vertex*> &m, string s) {
     MinPriorityQueue<Vertex*> pq;
     for (auto x : m) {
         if (x.first == s) {
             pq.push(x.second, 0);
-            // cout << "start vertex: " << s << endl;
         }
         else {
             x.second->weight = INT_MAX;
             pq.push(x.second, x.second->weight);
-            // cout << "other vertex: " << x.first << endl;
         }
     }
 
@@ -71,40 +49,22 @@ void dijsktras(unordered_map<string, Vertex*> &m, string s) {
 
         for (auto n : x->neighs) {
             if (x->weight + n.second < n.first->weight) {
-                // cout << "updating weight" << endl;
-                // cout << pq.size() << endl;
                 n.first->weight = x->weight + n.second;
                 n.first->bc = x;
                 pq.decrease_key(n.first,n.first->weight);
+                // cout << "updating weight of: " << n.first->row << ", " << n.first->col << "|| " << n.first->weight << endl;
             }
         }
     }
-
-    // for (auto x : m) {
-    //     cout << x.second->row << ", " << x.second->col << ": " << x.second->bc->row << ", " << x.second->bc->col << endl;
-    // }
-    // for (auto x : m) {
-    //     cout << x.second->row << ", " << x.second->col << ": " << x.second->weight << endl;
-    // }
 }
 void shortestPath(string start, string end, unordered_map<string, Vertex*> &m) {
-
-    //Step 0: Get the start/end vertices
+    
     Vertex* sPtr = m[start];
     Vertex* ePtr = m[end];
     sPtr->bc = nullptr;
 
-    //Step 1: Run BFS on start vertex
-    //to properly set breadcrumbs poineters
-    //encoding shortest paths.
-    // bfs(start, m);
     dijsktras(m, start);
-    // cout << "check 2" << endl;
-
-    //Step 2: Start at ePtr, following breadcrumbs
-    //back to sPtr, return path of vertices you
-    //visit.
-
+    
     Vertex* current = ePtr;
 
         while (current != nullptr)
@@ -114,10 +74,56 @@ void shortestPath(string start, string end, unordered_map<string, Vertex*> &m) {
         }
 }
 
+string rebuildSolved(unordered_map<string, Vertex*> m, string maze, string startPos, string endPos) {
+    int r = 0;
+    int c = 0;
+    string testing = "";
+    int verCount = 0;
+    shortestPath(startPos, endPos, m);
+    for (auto x : maze) {
+        if (x == '\n') {
+            r++;
+            c = -1;
+            testing = testing + x;
+        }
+        else {
+            if (x == ' ') {
+                string pos = to_string(r) + "," + to_string(c);
+                Vertex* checker = m[pos];
+                if (checker->path) {
+                    testing = testing + "o";
+                }
+                else {
+                    testing = testing + x;
+                }
+            }
 
+            if (x >= '0' && x <= '9') {
+                string pos = to_string(r) + "," + to_string(c);
+                Vertex* checker = m[pos];
+                if (checker->path) {
+                    testing = testing + "o";
+                }
+                else {
+                    testing = testing + x;
+                }
+            }
+            if (x == '#') {
+                testing = testing + x;
+            }
+            // c++;
 
+        }
+        c++;
+    }
+    maze = testing;
+    // for (auto x : maze) {
+    //     cout << x;
+    // }
+    return maze;
+}
 
-string solve(string maze) {
+string mazeLogic(unordered_map<string, Vertex*> graph, unordered_map<char, string> portals, string maze) {
     unordered_map<string, Vertex*> m;
     clearData(m);
     unordered_map<char, string> p;
@@ -145,8 +151,8 @@ string solve(string maze) {
         }
         counter++;
     }
+
     for(auto x : maze) {
-        // cout << "[" << r << "]" << "[" << c << "]";
         if (x == ' ' || (x >= '0' && x <= '9')) {
             Vertex* v = new Vertex(r, c);
             string pos = to_string(r) + "," + to_string(c); // was failing on the biggest 18 by 40 maze because r,c 1,11 is the same as 11, 1 so the comma is neccesary took forever to debug
@@ -174,16 +180,6 @@ string solve(string maze) {
             }
         }
 
-        if (x == ' ') {
-            if (flag == 0 && (c == 0 || r == 0 || c == maxCols - 1 || r == maxRows - 1)) {
-                startPos = to_string(r) + "," + to_string(c);
-                flag++;
-            }
-            else if (flag == 1 && (c == 0 || r == 0 || c == maxCols - 1 || r == maxRows - 1)) { //check all possible
-                endPos = to_string(r) + "," + to_string(c);
-                flag++;
-            }
-        }
 
         if (x == '\n') {
             r++;
@@ -193,63 +189,36 @@ string solve(string maze) {
         leftPos = "";
         upPos = "";
     }
+
     r = 0;
     c = 0;
-    string testing = "";
-    int verCount = 0;
-
-    shortestPath(startPos, endPos, m);
-    // for (auto x : m) {
-    //     if (x.second->marked) {
-    //         cout << x.second->row << ", " << x.second->col << endl;
-    //     }
-    // }
-
-    for (auto x : maze) {
-        if (x == '\n') {
-            r++;
-            c = -1;
-            testing = testing + x;
-        }
-
-        if (x == ' ') {
-            string pos = to_string(r) + "," + to_string(c);
-            Vertex* checker = m[pos];
-            if (checker->path) {
-                testing = testing + "o";
-            }
-            else {
-                testing = testing + x;
+    for (auto x : maze) { // changed logic to handle cases where the exits of the maze was a portal 
+    if (x == ' ' || (x >= '0' && x <= '9')) {
+        if (c == 0 || r == 0 || c == maxCols - 1 || r == maxRows - 1) {
+            if (flag == 0) {
+                startPos = to_string(r) + "," + to_string(c);
+                
+                flag++;
+            } else if (flag == 1) {
+                endPos = to_string(r) + "," + to_string(c);
+               
+                flag++;
             }
         }
-
-        if (x >= '0' && x <= '9') {
-            string pos = to_string(r) + "," + to_string(c);
-            Vertex* checker = m[pos];
-            if (checker->path) {
-                testing = testing + "o";
-            }
-            else {
-                testing = testing + x;
-            }
-        }
-        if (x == '#') {
-            testing = testing + x;
-        }
-        c++;
     }
-    maze = testing;
-    for (auto x : maze) {
-        cout << x;
+    if (x == '\n') {
+        r++;
+        c = -1;
     }
+    c++;
+    
+    }
+    return rebuildSolved(m, maze, startPos, endPos);
+}
 
-    // for (auto x : m) {
-    //     cout << x.first << ": ";
-    //     for (auto i : x.second->neighs) {
-    //         // cout << i.first->row << " " <<  i.first->col << ", ";
-    //         cout << i.second << ",";
-    //     }
-    //     cout << endl;
-    // }
-    return maze;
+string solve(string maze) {
+    unordered_map<string, Vertex*> m;
+    unordered_map<char, string> p;
+    clearData(m);
+    return mazeLogic(m, p, maze);
 }
